5ee42cba0b7b1badd07e705164781d2aff8af007
Pyarelal Knowles <heuristic42@gmail.com>
2018-12-01 17:30:23 -0800

    merged character classes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(function(){
     var game = new GameWorld();
     for (var i = 0; i < 5; ++i) {
-        game.characters.push(new Character("somerole"));
+        game.characters.push(new Character("somerole", "someability"));
     }
     game.display();
 });
